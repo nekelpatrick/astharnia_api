1570004fe59bbe8137066452d1d69b8ce8eee281
Patrick Nekel <patric@kenzie.com.br>
2021-11-04 23:17:30 -0300

    :recycle: Fix: characters main route and express server name

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require("express");
-const server = express();
-server.use(express.json());
+const app = express();
+app.use(express.json());
 
 const mainCharacters = ["Redmund", "Baldur", "Altuin", "Silliris"];
 //
-server.get("/mainCharacters/:index", (req, res) => {
+app.get("/main-characters/:index", (req, res) => {
   const { index } = req.params;
   return res.json(mainCharacters[index]);
 });
 
-server.get("/mainCharacters", (req, res) => {
+app.get("/main-characters", (req, res) => {
   return res.json(mainCharacters);
 });
 
-server.post("/mainCharacters", (req, res) => {
+app.post("/main-characters", (req, res) => {
   const { name } = req.body;
   mainCharacters.push(name);
 
   return res.json(mainCharacters);
 });
 
-server.put("/mainCharacters/:index", (req, res) => {
+app.put("/main-characters/:index", (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -28,11 +28,11 @@ server.put("/mainCharacters/:index", (req, res) => {
   return res.json(mainCharacters);
 });
 
-server.delete("/mainCharacters", (req, res) => {
+app.delete("/main-characters", (req, res) => {
   const { index } = req.params;
 
   mainCharacters.splice(index, 1);
   return res.json({ message: "O personagem foi deletado" });
 });
 
-server.listen(3000);
+app.listen(3000);
